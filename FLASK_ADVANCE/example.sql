eae4c5f0c462a8f87fd3da565df86c95ec72df83
Butian <88835096+saliteta@users.noreply.github.com>
2022-11-22 11:22:09 +0800

    Add files via upload

---
+++ FLASK_ADVANCE/example.sql
@@ -0,0 +1,4 @@
+CREATE TABLE table1(fname text NOT NULL, lname text NOT NULL);
+INSERT INTO table1 VALUES("Victor", "Soi")
+INSERT INTO table1 VALUES("Saliteta", "Butian")
+INSERT INTO table1 VALUES("Namori", "Akina")
